71f555819e8d47706de14387c1978fbf53dbf497
Maro Egweni <youremail@example.com>
2026-03-06 14:59:04 -0500

    fix: enable SSL for render PostgreSQL connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,5 +3,8 @@ import pkg from 'pg';
 const {Pool} = pkg;
 
 export const pool = new Pool({
-   connectionString:process.env.DATABASE_URL
+   connectionString:process.env.DATABASE_URL,
+   ssl: {
+      rejectedUnauthorized: false
+   }
 });
